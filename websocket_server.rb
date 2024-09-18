dfd310ab945d18c1e10db045df0948f64343502f
ikalco <73481042+ikalco@users.noreply.github.com>
2024-09-18 09:54:54 -0500

    add broadcasting to all clients text and binary

--- websocket_server.rb
+++ websocket_server.rb
@@ -290,6 +290,18 @@ class WebSocketServer
         other_socket.send_close(1008) if other_socket
     end
 
+    def broadcast_text(data)
+        @clients.each do |client|
+            client.send_text(data)
+        end
+    end
+
+    def broadcast_binary(data)
+        @clients.each do |client|
+            client.send_binary(data)
+        end
+    end
+
     def start()
         while !@server.closed? do
             now = Time.now
